3b297e0f8f352b6322b8db4c074721da7dfed39b
Erich L <42230060+Eman2022@users.noreply.github.com>
2019-12-03 16:58:25 +0800

    added looking for bad actors
    can see if someone is logging into someone else's account from their own computer

--- js/employee.js
+++ js/employee.js
@@ -4,68 +4,125 @@ function Employee(id){
 	this.daysAtWork = 0;
 	this.totalMinutesWorked = 0;
 	this.totalWorkOutput = 0.0;
+	this.mainIP = "undefined"; //自己的IP地址
+	
 	this.dangerLevel = 0.0;
+	this.totalAddedDanger = 0.0;
+	
 	this.workReports = {};
 	this.ipUse = {};
+	this.ipAccess = {};
 	this.talksTo = {};
 	
 	this.getDaysWorked = function(){
 		return this.daysAtWork;
 	}
 	
-	this.examineUsedIP = function(ip){
+	
+	this.examineLoginFailue = function(accessDate, login, usedIPisMyIp){
+		//proto, dip, dport, sip, sport, state, time, user
+		
+		this.punish(accessDate, punishment.logInFail);	
+		this.workReports[accessDate].failedLogins += 1;
+	}
+	
+	this.examineUsedIP = function(dateEst, ip){
+		var myMainIP; var uses = 0;
+		for (let [key, value] of Object.entries(this.ipUse)) {
+			if(value > uses) {
+				uses = value;
+				myMainIP = key;
+			}
+		}
+		this.mainIP = myMainIP;
+		//console.log("my main IP predicted for " + this.id + ": " + myMainIP);
+		
 		if((ip in this.ipUse)){ //记录用的IP地址
 			this.ipUse[ip] += 1;
 		} else {
 			this.ipUse[ip] = 1;
-			this.dangerLevel += punishment.usedNewIP;
+			if(this.daysKnown > 2){
+				this.punish(dateEst, punishment.usedNewIP);
+			}
+		}
+	}
+	
+	this.examineAccessedIP = function(dateEst, ip){
+		if((ip in this.ipAccess)){ //记录打开的IP地址
+			this.ipAccess[ip] += 1;
+		} else {
+			this.ipAccess[ip] = 1;
+			if(this.daysKnown > 2){
+				this.punish(dateEst, punishment.accessedNewIP);
+			}
+		}
+	}
+	
+	this.punish = function(dateIn, amount){ //惩罚此员工
+		this.dangerLevel += amount;
+		this.totalAddedDanger += amount;
+		if(!(dateIn in this.workReports)){
+			var nwr = new WorkReport(dateIn, 0, false);
+			nwr.addDanger(amount);
+			this.workReports[dateIn] = nwr;
+		} else {
+			this.workReports[dateIn].addDanger(amount);
+		}
+	}
+	
+	this.reward = function(dateIn, amount){ //表扬此员工
+		this.totalWorkOutput += amount;
+		
+		if(!(dateIn in this.workReports)){
+			var nwr = new WorkReport(dateIn, 0, false);
+			nwr.addReward(amount);
+			this.workReports[dateIn] = nwr;
+		} else {
+			this.workReports[dateIn].addReward(amount);
 		}
 	}
 	
 	this.addLoginReport = function(login){
 		//proto, dip, dport, sip, sport, state, time, user
-		this.examineUsedIP(login.sip);
+		var loginSuccess = login.state.charAt(0) === 's';
+		var usedIPisMyIp = login.sip === this.mainIP;
+		var dateparts = login.time.split(" ");	
+		var accessDate = dateparts[0];
 		
-		var dateparts = login.time.split(" ");	//增加工作量
-		var sentDate = dateparts[0];
+		this.examineUsedIP(accessDate, login.sip); //记录使用的IP地址
 		
-		if(!(sentDate in this.workReports)){
-			var nwr = new WorkReport(sentDate, 0, false);
-			if(login.state.charAt(0) === 's'){
-				nwr.workOutput = rewards.loggedIn;
-			} else {
-				this.dangerLevel += punishment.logInFail;
-				nwr.failedLogins = 1;
-			}
-			this.workReports[sentDate] = nwr;
-			this.totalWorkOutput += nwr.workOutput;
+		this.examineAccessedIP(accessDate, login.dip);//记录访问的IP地址
+		
+		if(loginSuccess){
+			if(!usedIPisMyIp && this.daysKnown > 2) console.log("successful HACK DETECTED getting into " + login.user);
+			this.reward(accessDate, rewards.loggedIn);	//增加工作量
 		} else {
-			if(login.state.charAt(0) === 's'){
-				this.workReports[sentDate].workOutput += rewards.loggedIn;
-				this.totalWorkOutput += rewards.loggedIn;
-			} else {
-				this.dangerLevel += punishment.logInFail;
-				this.workReports[sentDate].logInFail += 1;
-			}
+			this.examineLoginFailue(accessDate, login, usedIPisMyIp);//检查登录失败
+			if(!usedIPisMyIp && this.daysKnown > 2){
+				var badguy = "?";
+				if(typeof manager !== "undefined"){
+					badguy = manager.findEmployeeByMainIp(login.sip).id;
+				}
+				console.log("unsuccessful HACK DETECTED");
+				console.log(badguy + " trying to get into " + login.user);
+				
+			} 
 		}
+		
 	}
 	
 	this.addEmailReport = function(email){
 		//time, proto, sip, sport, dip, dport, from, to, subject
 		
-		this.examineUsedIP(email.sip);
-		
 		var dateparts = email.time.split(" ");	//增加工作量
 		var sentDate = dateparts[0];
-		if(!(sentDate in this.workReports)){
-			var nwr = new WorkReport(sentDate, 0, false);
-			nwr.workOutput = rewards.sentEmail;
-			this.workReports[sentDate] = nwr;
-		} else {
-			this.workReports[sentDate].workOutput += rewards.sentEmail;
-		}
+		this.reward(sentDate, rewards.sentEmail);
 		
-		
+		this.examineUsedIP(sentDate, email.sip); //记录使用的IP地址
+//		if(email.from.split("@")[0] === "1348"){
+//			console.log("our guy sent an email! : " + this.totalWorkOutput);
+//		}
+	
 		if(email.to.indexOf(";") > 0){ //记录发给谁
 			var sentToList = [];
 			email.to.split(";").forEach(function(address){
@@ -96,7 +153,8 @@ function Employee(id){
 		}
 	}
 	
-	this.addWorkReport = function(workReport){
+	this.addWorkReport = function(workReport){//打卡信息
+		
 		var thisReport = this.workReports[workReport.estDate];
 		if(typeof thisReport !== "undefined"){
 			thisReport.minutesWorked = workReport.minutesWorked;
@@ -109,7 +167,8 @@ function Employee(id){
 			if(workReport.present) this.daysAtWork++;
 		}
 		if(!workReport.present){
-			this.dangerLevel += punishment.missedWork;
+			//this.dangerLevel += punishment.missedWork;
+			this.punish(workReport.estDate, punishment.missedWork);
 			var parts = workReport.estDate.split("-");
 			var day = parseInt(parts[2]);
 			if(day > 1){
@@ -121,7 +180,7 @@ function Employee(id){
 				var yesterdayReport = this.workReports[yesterdayString];
 				if(typeof yesterdayReport !== "undefined"){
 					if(!yesterdayReport.present){
-						this.dangerLevel += punishment.missedTwoDays;
+						this.punish(workReport.estDate, punishment.missedTwoDays)
 					}
 				}
 			}
