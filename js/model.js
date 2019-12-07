0f28b97b1c00dc6a1e6c6c1d4bfbfbaf5e09d452
Erich L <42230060+Eman2022@users.noreply.github.com>
2019-12-07 09:23:43 +0800

    update to dsv4
    go go go

--- js/model.js
+++ js/model.js
@@ -21,11 +21,22 @@ function WorkReport(date, minutes, present){
 	this.workOutput = 0;
 	this.failedLogins = 0;
 	this.danger = 0;
+	this.alerts = [];
 	
 	this.addDanger = function(amount){
 		this.danger += amount;
+		return this;
 	}
 	this.addReward = function(amount){
 		this.workOutput += amount;
+		return this;
+	}
+	
+	this.addAlert = function(alert){
+		this.alerts.push(alert);
+	}
+	
+	this.getAlerts = function(){
+		return this.alerts;
 	}
 }
