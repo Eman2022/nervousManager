134f58be83245d9c14cf6db686083899bb732360
Erich L <42230060+Eman2022@users.noreply.github.com>
2019-12-13 15:12:14 +0800

    version 0.5
    the main man manager

--- js/nervousManager.js
+++ js/nervousManager.js
@@ -1,16 +1,22 @@
 
 	//Nervous Manager
 	
-	//my goal： 抓住员工1487 
+	//my goal： 抓住员工1487 和 1376
 	//1487 logged into accounts 1211, 1080, 1228
+	//17,21,27,30号 data sent to outside servers
+	//1376 tries to delete database data
+	//VPN remote access: 1147, 1283, 1284, 1328, 1334, 1376, 1487, 1494 
+	//on 11-24 13.250.177.223 gets data sent to it by internal server
+	
+	//1389, 1383, 1352, 1149
 function NervousManager (name){
 	
 	this.name = name; //name of this manager
 	totalEmployees = 0;
 	employees = {};
+	servers = {};
 	this.lastDayExamined = 0;
-
-
+	this.managerBusy = false;
 	
 	this.findEmployeeByMainIp = function(ip){
 		var empsWithThisMainIP = [];
@@ -52,9 +58,21 @@ function NervousManager (name){
 				
 				employees[key].dangerLevel = danger;
 				employees[key].workOutput = workOutput;
-;				employee.criticalMessage = "";
+				employee.criticalMessage = "";
 				//console.log(" to: " + employees[key].dangerLevel);
 			}
+		}
+		for(let [key, server] of Object.entries(servers)) {
+			
+			server.mostRecentUsers = [];
+		}
+	}
+	
+	function examineServer(ip){//确定一个服务器是存在的。不存在的话：创建
+		if(ip in this.servers){
+			
+		} else {
+			this.servers[ip] = new Server(ip);
 		}
 	}
 	
@@ -105,7 +123,10 @@ function NervousManager (name){
 		//proto, dip, dport, sip, sport, state, time, user
 		if(login){
 			if(login.user.charAt(0) !== "r" && !isNaN(login.user)){//ignore root
-				//console.log(" not ignoring " + login.user);
+				if(isCompanyServer(login.dip)){
+					examineServer(login.dip);
+					servers[login.dip].addLoginReport(login);
+				}
 				examineEmployeeID(login.user);
 				employees[login.user].addLoginReport(login);
 			}
@@ -116,10 +137,39 @@ function NervousManager (name){
 		//stime, dtime, proto, dip, dport, sip, sport, uplink_length, downlink_length
 		if(report){
 			if(manager){
-				var likelyEmployee = manager.findEmployeeByMainIp(report.sip);
-				if(likelyEmployee) likelyEmployee.addTCPReport(report);
+				if(isCompanyServer(report.sip)){
+					examineServer(report.sip);
+					servers[report.sip].addTCPReport(report);
+				} else {
+					var likelyEmployee = manager.findEmployeeByMainIp(report.sip);
+					if(likelyEmployee){
+						if(likelyEmployee.daysAtWork > 1){
+							likelyEmployee.addTCPReport(report);
+							//console.log("got " + report.proto + " log for " +likelyEmployee.id);
+						}
+					} else {
+						if(report.dip === "13.250.177.223"){
+							console.log("MISSED VERY IMPORTANT TRANSFER");//on 11-24 13.250.177.223
+						}
+					}
+				}
+
 			}
-			
+		}
+	}
+	
+	function examineWebLog(log){
+		//time, sip, sport, dip, dport, host
+		if(log){
+			if(manager){
+				var likelyEmployee = manager.findEmployeeByMainIp(log.sip);
+				if(likelyEmployee){
+					if(likelyEmployee.getDaysWorked() > 1){
+//						console.log("who uses " +log.sip + ": " + likelyEmployee.id + " -> " + log.host);
+						likelyEmployee.addWebLog(log);
+					}
+				}
+			}
 		}
 	}
 	
@@ -136,7 +186,6 @@ function NervousManager (name){
 					examineLogin(d);
 				});
 			});
-
 		}
 	}
 	
@@ -188,11 +237,43 @@ function NervousManager (name){
 					var char0 = d.proto.charAt(0);
 					if(char0 === 's' && d.proto.charAt(1) === 's' || char0 === 'f'){
 						if(parseInt(d.downlink_length) > 15000 || parseInt(d.uplink_length) > 15000){
+							d.downlink_length = parseInt(d.downlink_length);
+							d.uplink_length = parseInt(d.uplink_length);
 							examineTCPReport(d);
 						}
 					}
 				});
 			});
+		}
+		return this;
+	}
+	
+	this.loadWebLogCSV = function(day){
+		var dayString = "0";
+		if(typeof day !== "undefined"){
+			if(day < 10){
+				dayString = dayString + day;
+			} else {
+				dayString = day;
+			}
+			
+			d3.csv("csv/2017-11-"+dayString+"/weblog.csv", function(data) {
+				data.forEach(function(d){
+					//time, sip, sport, dip, dport, host
+				if(d.host.length > 0){ //不看没有网址的
+					var char0 = d.host.charAt(0);
+					if(char0 !== 'e' && d.host.charAt(1) !== 'm'){ //不看email的
+						if(char0 !== 'O' && d.host.charAt(1) !== 'A'){//不看OA内部的
+							examineWebLog(d);
+						}
+					}
+				}
+				});
+				manager.managerBusy = false;
+				triggerFindDangerousEmployees();
+				
+			});
+
 		}
 		return this;
 	}
@@ -233,7 +314,7 @@ function NervousManager (name){
 		return dangerousEmployees.reverse();
 	}
 	
-	this.printOutFailedLogins = function(employees){
+	this.printOutFailedLogins = function(employees){//not in use
 		for (let [key, value] of Object.entries(employees)) {
 			var employii = value;
 		  for (let [key2, value2] of Object.entries(employii.workReports)) {
